d4d56453e372d36451d6a9a50581c42d86021705
Rohmat <147891420+RohmatIF@users.noreply.github.com>
2024-01-02 18:45:45 +0700

    Update streamlit(uas).py

--- streamlit(uas).py
+++ streamlit(uas).py
@@ -10,8 +10,9 @@ unique_items = list(set(item for sublist in itemsets for item in sublist))
 transaction_matrix = pd.DataFrame(0, columns=unique_items, index=range(len(transactions)))
 
 for i, transaction in enumerate(transactions):
-     for item in transaction:
-        transaction_matrix.loc[i, item] = 1
+    items = transaction.split(',')
+    transaction_matrix.loc[i, items] = 1
+
 
   
 st.title('Analisis Tranjjjsaksi ')
